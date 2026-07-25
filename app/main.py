0153e8a76eeea48fa512ff60eda2c90f2db2a9e1
elmaniouiSoulaimane <elmanioui.soulaimane@gmail.com>
2026-07-25 19:18:14 +0100

    [fix]:import models in main.py so create_all registers the Document table

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from .database import init_db
 from contextlib import asynccontextmanager
+from app import models
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
